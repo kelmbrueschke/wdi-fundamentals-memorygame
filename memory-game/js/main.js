b7f6c0839ca3a61897927b6d33fab63505454d39
Brueschke Kelm P <BrueschkeKelmP@JohnDeere.com>
2020-01-22 11:31:06 -0600

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,31 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+let cards = ["queen", "queen", "queen", "queen"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen");
+
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped king");
+
+cardsInPlay.length;
+
+if (cardsInPlay.length === 2) {
+	console.log("User flipped 2 cards");
+}
+	else {
+		console.log("You must flip 2 cards");
+	}
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} 
+	else { 
+		alert("Sorry, try again.");
+}
